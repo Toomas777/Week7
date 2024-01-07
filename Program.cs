61fff8c4a51635b1a056659f66ef1075abc7da93
Toomas J. <tom.juri@hotmail.com.>
2024-01-07 23:25:24 +0200

    add week7 code

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Are You coming or Leaving? (in/out)");
+string userChoice = Console.ReadLine();
+
+
+
+
+if (userChoice == "in")
+{
+    PrintHello();
+}
+else
+{
+    PrintGoodBye();
+}
+
+
+static void PrintHello()
+{
+
+    Console.WriteLine("Hello world!");
+}
+
+
+
+static void PrintGoodBye()
+{
+
+    Console.WriteLine("See you later, aligator!");
+}
+
